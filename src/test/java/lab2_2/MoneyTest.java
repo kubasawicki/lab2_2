07de4e307d10f7f93310c5b21d4a434173182b6a
Sebastian Piecowski <202268@edu.p.lodz.pl>
2018-03-22 22:21:41 +0100

    Dodano addMoneyWithCompatibleCurrencyTest

--- src/test/java/lab2_2/MoneyTest.java
+++ src/test/java/lab2_2/MoneyTest.java
@@ -30,5 +30,11 @@ public class MoneyTest {
     	Money moneyWithDifferentCurrency=new Money(new BigDecimal(10),currency);
     	money.add(moneyWithDifferentCurrency);
     }
-
+    @Test
+    public void addMoneyWithCompatibleCurrencyTest() {
+    	Money money=new Money(new BigDecimal(10), Currency.getInstance("EUR"));
+    	Money money2=new Money(new BigDecimal(100), Currency.getInstance("EUR"));
+    	assertThat(money.add(money2), Matchers.is(new Money(new BigDecimal(110), Currency.getInstance("EUR"))));
+    }
+    
 }
