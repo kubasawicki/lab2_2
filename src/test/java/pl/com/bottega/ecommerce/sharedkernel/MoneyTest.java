cb43370f547fcbf7fcbed1bd103c9f7f848d4ab0
Parada <paradowskim94@gmail.com>
2018-03-23 15:01:06 +0100

    Added test checking, if adding money works

--- src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
@@ -9,12 +9,17 @@ import static org.junit.Assert.*;
 public class MoneyTest {
 
     Money pln1 = new Money(10,"PLN");
-
+    Money pln2 = new Money(25,"PLN");
     @Test
     public void multiplierExpectedValueTest(){
         Money expected = new Money(100, "PLN");
         assertThat(pln1.multiplyBy(10), is(equalTo(expected)));
 
+    }
+    @Test
+    public void addingValuesTest(){
+        Money expected = new Money(35,"PLN");
+        assertThat(pln1.add(pln2), is(equalTo(expected)));
     }
 
 }
