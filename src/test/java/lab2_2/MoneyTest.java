952b6e96b501532ca3cc36e0c499b9bf80ba49bd
liwocha.dawid <202214@edu.p.lodz.pl>
2018-03-23 11:19:11 +0100

    added MoneyTest class and test for multiplyBy with BigDecimal as argument

---
+++ src/test/java/lab2_2/MoneyTest.java
@@ -0,0 +1,22 @@
+package lab2_2;
+
+import static org.junit.Assert.assertThat;
+
+import java.math.BigDecimal;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+public class MoneyTest {
+
+    @Test
+    public void moneyMultiplyByBigDecimalShouldReturnSameResultAsBigDecimalMultiplyMethod() {
+        Money money = new Money(new BigDecimal(50.25));
+        BigDecimal value = new BigDecimal(3.22);
+        Money expectedMoney = new Money(new BigDecimal(50.25).multiply(new BigDecimal(3.22)));
+        assertThat(money.multiplyBy(value), Matchers.is(expectedMoney));
+    }
+    
+}
