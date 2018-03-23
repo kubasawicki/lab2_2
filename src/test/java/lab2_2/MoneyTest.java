9139db929ea7b76fc6f0bba69b5524e2d4787f2d
liwocha.dawid <202214@edu.p.lodz.pl>
2018-03-23 11:55:49 +0100

    code formatting

--- src/test/java/lab2_2/MoneyTest.java
+++ src/test/java/lab2_2/MoneyTest.java
@@ -19,7 +19,7 @@ public class MoneyTest {
         Money expectedMoney = new Money(new BigDecimal(50.25).multiply(new BigDecimal(3.22)));
         assertThat(money.multiplyBy(value), Matchers.is(expectedMoney));
     }
-    
+
     @Test
     public void moneyMultiplyByDoubleShouldReturnSameResultAsBigDecimalMultiplyMethod() {
         Money money = new Money(new BigDecimal(27.14));
@@ -27,7 +27,7 @@ public class MoneyTest {
         Money expectedMoney = new Money(new BigDecimal(27.14).multiply(new BigDecimal(4.92)));
         assertThat(money.multiplyBy(value), Matchers.is(expectedMoney));
     }
-    
+
     @Test
     public void moneyAddedToOtherMoneyInTheSameCurrencyShouldBeAdded() {
         Money firstMoneyEUR = new Money(new BigDecimal(20.25));
@@ -35,14 +35,14 @@ public class MoneyTest {
         Money expectedMoney = new Money(new BigDecimal(20.25 + 14.50));
         assertThat(firstMoneyEUR.add(secondMoneyEUR), Matchers.is(expectedMoney));
     }
-    
+
     @Test(expected = IllegalArgumentException.class)
     public void moneyAddedToOtherMoneyInOtherCurrencyShouldThrowException() {
         Money moneyEUR = new Money(new BigDecimal(20.25));
         Money moneyUSD = new Money(new BigDecimal(14.50), Currency.getInstance("USD"));
         moneyEUR.add(moneyUSD);
     }
-    
+
     @Test
     public void moneySubtractedFromOtherMoneyInTheSameCurrencyShouldBeSubtracted() {
         Money firstMoneyEUR = new Money(new BigDecimal(24.75));
@@ -50,35 +50,35 @@ public class MoneyTest {
         Money expectedMoney = new Money(new BigDecimal(24.75 - 12.20));
         assertThat(firstMoneyEUR.subtract(secondMoneyEUR), Matchers.is(expectedMoney));
     }
-    
+
     @Test(expected = IllegalArgumentException.class)
     public void moneySubtractedFromOtherMoneyInOtherCurrencyShouldThrowException() {
         Money moneyEUR = new Money(new BigDecimal(24.75));
         Money moneyUSD = new Money(new BigDecimal(12.20), Currency.getInstance("USD"));
         moneyEUR.subtract(moneyUSD);
     }
-    
+
     @Test
     public void greaterMoneyShouldBeGreater() {
         Money firstMoney = new Money(new BigDecimal(22.20));
         Money secondMoney = new Money(new BigDecimal(22.19));
         assertThat(firstMoney.greaterThan(secondMoney), Matchers.is(true));
     }
-    
+
     @Test
     public void lessMoneyShouldBeLess() {
         Money firstMoney = new Money(new BigDecimal(21.47));
         Money secondMoney = new Money(new BigDecimal(22.19));
         assertThat(firstMoney.lessThan(secondMoney), Matchers.is(true));
     }
-    
+
     @Test
     public void equalMoneyShouldBeLessOrEquals() {
         Money firstMoney = new Money(new BigDecimal(12.17));
         Money secondMoney = new Money(new BigDecimal(12.17));
         assertThat(firstMoney.lessOrEquals(secondMoney), Matchers.is(true));
     }
-    
+
     @Test
     public void lessMoneyShouldBeLessOrEquals() {
         Money firstMoney = new Money(new BigDecimal(12.15));
