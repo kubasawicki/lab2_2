416a196afe4d0618be5ff2e183f3831819b5d393
Kamil Błażejczuk <202119@edu.p.lodz.pl>
2018-03-24 12:06:13 +0100

    Testowanie klasy money - Mtesty metody subtract

--- src/test/java/lab2_2/MoneyTests.java
+++ src/test/java/lab2_2/MoneyTests.java
@@ -45,4 +45,23 @@ public class MoneyTests {
         final Money EXPECTED = new Money(new BigDecimal(110.37 + 39.63), currencyPLN);
         assertThat(EXPECTED, is(moneyUSD.add(moneyPLN)));      
 	}	
+	
+	@Test
+	public void MoneySubtractedFromMoneyWithTheSameCurrencyShouldReturnSameValueAsAddition() {
+		Currency currency = Currency.getInstance("PLN");
+		Money money = new Money(new BigDecimal(1200.50), currency);
+        Money money2 = new Money(new BigDecimal(200.50), currency);
+        final Money EXPECTED = new Money(new BigDecimal(1200.50 - 200.50), currency);
+        assertThat(EXPECTED, is(money.subtract(money2)));
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void MoneySubtractedFromMoneyWithAnotherCurrencyShouldThrowIllegalArgumentException() {
+		Currency currencyPLN = Currency.getInstance("PLN");
+		Currency currencyUSD = Currency.getInstance("USD");
+		Money moneyUSD = new Money(new BigDecimal(1200.50), currencyUSD);
+        Money moneyPLN = new Money(new BigDecimal(200.50), currencyPLN);
+        final Money EXPECTED = new Money(new BigDecimal(1200.50 - 200.50), currencyPLN);
+        assertThat(EXPECTED, is(moneyUSD.subtract(moneyPLN)));      
+	}
 }
