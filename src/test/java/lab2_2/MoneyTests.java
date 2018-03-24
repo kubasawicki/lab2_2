fb7acf65f965c7056ca1511d6955e935c3f8cad0
Kamil Błażejczuk <202119@edu.p.lodz.pl>
2018-03-24 11:47:20 +0100

    Testowanie klasy money - Mtest metody add dla 2 wartości z tą samą walutą

--- src/test/java/lab2_2/MoneyTests.java
+++ src/test/java/lab2_2/MoneyTests.java
@@ -10,7 +10,7 @@ import pl.com.bottega.ecommerce.sharedkernel.Money;
 public class MoneyTests {
 
 	@Test
-	public void MultiplyByTestBigDecimal() {
+	public void MultiplyByTestBigDecimalShouldReturnSameValueAsMultiply() {
 		Money money = new Money(new BigDecimal(10.50));
         BigDecimal value = new BigDecimal(5.50);
         final Money EXPECTED = new Money(new BigDecimal(10.50).multiply(new BigDecimal(5.50)));
@@ -18,10 +18,19 @@ public class MoneyTests {
 	}
 	
 	@Test
-	public void MultiplyByTestDouble() {
+	public void MultiplyByTestDoubleShouldReturnSameValueAsMultiply() {
 		Money money = new Money(35.20);
         double value = 2.75;
         final Money EXPECTED = new Money(new BigDecimal(35.20).multiply(new BigDecimal(2.75)));
         assertThat(EXPECTED, is(money.multiplyBy(value)));
 	}
+	
+	@Test
+	public void MoneyAddedToMoneyWithTheSameCurrencyShouldReturnSameValueAsAddition() {
+		Money money = new Money(new BigDecimal(110.37));
+        Money money2 = new Money(new BigDecimal(39.63));
+        final Money EXPECTED = new Money(new BigDecimal(110.37 + 39.63));
+        assertThat(EXPECTED, is(money.add(money2)));
+	}
+	
 }
