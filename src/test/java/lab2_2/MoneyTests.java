4f24e359ad1381899f4d4848f98fcbd6da057707
Kamil Błażejczuk <202119@edu.p.lodz.pl>
2018-03-24 11:40:11 +0100

    Testowanie klasy money - MultiplyByTest dla typów BigDecimal i double

--- src/test/java/lab2_2/MoneyTests.java
+++ src/test/java/lab2_2/MoneyTests.java
@@ -10,11 +10,18 @@ import pl.com.bottega.ecommerce.sharedkernel.Money;
 public class MoneyTests {
 
 	@Test
-	public void MultiplyByTest() {
+	public void MultiplyByTestBigDecimal() {
 		Money money = new Money(new BigDecimal(10.50));
         BigDecimal value = new BigDecimal(5.50);
         final Money EXPECTED = new Money(new BigDecimal(10.50).multiply(new BigDecimal(5.50)));
         assertThat(EXPECTED, is(money.multiplyBy(value)));
 	}
-
+	
+	@Test
+	public void MultiplyByTestDouble() {
+		Money money = new Money(35.20);
+        double value = 2.75;
+        final Money EXPECTED = new Money(new BigDecimal(35.20).multiply(new BigDecimal(2.75)));
+        assertThat(EXPECTED, is(money.multiplyBy(value)));
+	}
 }
