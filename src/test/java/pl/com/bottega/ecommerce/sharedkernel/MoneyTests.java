dee717d49de9926549cb50f78880672e6ca9eb8c
Cezary Witkowski <czarecoo@gmail.com>
2018-03-26 13:52:41 +0200

    Changed test names to be more informative. Added 4 more tests (negative values, multiplying, greaterThan and lessThan)

--- src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTests.java
+++ src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTests.java
@@ -3,6 +3,7 @@ package pl.com.bottega.ecommerce.sharedkernel;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.math.BigDecimal;
 import java.util.Currency;
 
 import org.junit.BeforeClass;
@@ -12,33 +13,60 @@ public class MoneyTests {
     static Money oneHundredPLN;
     static Money oneHundredEUR;
     static Money twentyEUR;
+    static Money zero;
 
     @BeforeClass
-    public static void createMoney() {
+    public static void createFewMoneyObjects() {
         oneHundredPLN = new Money(100, "PLN");
         oneHundredEUR = new Money(100, Currency.getInstance("EUR"));
         twentyEUR = new Money(20);
+        zero = new Money(0);
     }
 
     @org.junit.Test
-    public void addTwoMoneyObjects() {
+    public void addingTwoMoneyObjectsShouldEqualObjectWithAddedValue() {
         Money oneHundredTwentyEUR = new Money(120);
         assertThat(oneHundredEUR.add(twentyEUR).equals(oneHundredTwentyEUR), is(true));
     }
 
     @org.junit.Test
-    public void subtractTwoMoneyObjects() {
+    public void subtractingTwoMoneyObjectsShouldEqualObjectWithSubstractedValue() {
         Money eightyEUR = new Money(80);
         assertThat(oneHundredEUR.subtract(twentyEUR).equals(eightyEUR), is(true));
     }
 
     @org.junit.Test(expected = IllegalArgumentException.class)
-    public void addTwoDifferentCurrencyMoneyObjects() {
+    public void addingTwoDifferentCurrencyMoneyObjectsShouldCauseException() {
         oneHundredPLN.add(oneHundredEUR);
     }
 
     @org.junit.Test(expected = IllegalArgumentException.class)
-    public void subtractTwoDifferentCurrencyMoneyObjects() {
+    public void subtractingTwoDifferentCurrencyMoneyObjectsShouldCauseException() {
         oneHundredPLN.subtract(oneHundredEUR);
     }
+
+    @org.junit.Test
+    public void substractingHigherValueMoneyFromLowerValueMoneyShouldEqualObjectWithMinusValue() {
+        Money minusEightyEUR = new Money(-80);
+        assertThat(twentyEUR.subtract(oneHundredEUR).equals(minusEightyEUR), is(true));
+    }
+
+    @org.junit.Test
+    public void moneyMultiplyingMethodShouldReturnEqualResultAsBigDecimalMultiplyMethod() {
+        Money money = new Money(new BigDecimal(100));
+        BigDecimal multiplyValue = new BigDecimal(2.5);
+
+        assertThat(money.multiplyBy(multiplyValue), is(new Money(new BigDecimal(100).multiply(new BigDecimal(2.5)))));
+    }
+
+    @org.junit.Test
+    public void oneHundredEuroShouldBeGreaterThanTwentyEuro() {
+        assertThat(oneHundredEUR.greaterThan(twentyEUR), is(true));
+    }
+
+    @org.junit.Test
+    public void oneHundredEuroShouldBeLessThanTwentyEuro() {
+        assertThat(twentyEUR.lessThan(oneHundredEUR), is(true));
+    }
+
 }
