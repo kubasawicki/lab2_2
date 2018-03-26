74af7efb8e54c03927c44ca412979d0b7c9ac6dd
Jakub Mankowski <jakub.mankowski20@gmail.com>
2018-03-27 01:18:03 +0200

    Test Money subtract in same and other Currency

--- src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
@@ -31,5 +31,20 @@ public class MoneyTest {
         priceEUR1.add( priceEUR2 );
     }
 
+    @Test
+    public void subtract() throws Exception {
+        Money priceEUR1 = new Money( 23.57 );
+        Money priceEUR2 = new Money( 10.33 );
+        Money expectedValue = new Money( 23.57 - 10.33, "EUR" );
+        assertThat( priceEUR1.subtract( priceEUR2 ), is( expectedValue ) );
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void substractPriceInOtherCurrency() throws Exception {
+        Money priceEUR1 = new Money( 23.57 );
+        Money priceEUR2 = new Money( 10.33 );
+        priceEUR1.add( priceEUR2 );
+    }
+
 
 }
