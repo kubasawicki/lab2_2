8b375d007c0bb132d3556ee374f7f921fc1b30eb
Jakub Sawicki <202289@edu.p.lodz.pl>
2018-04-16 16:13:16 +0200

    MoneyTest. java class fix

--- src/test/java/lab2_2/MoneyTest.java
+++ src/test/java/lab2_2/MoneyTest.java
@@ -1,24 +1,8 @@
 package lab2_2;
 
-<<<<<<< HEAD
-public class MoneyTest {
-
-	
-}
-=======
-import static org.junit.Assert.assertThat;
-import java.math.BigDecimal;
-import org.hamcrest.Matchers;
-import org.junit.BeforeClass;
-import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 public class MoneyTest {
 
-@BeforeClass
-public static void setUpBeforeClass() throws Exception {
-	Money money=new Money(new BigDecimal(10), null);
-	assertThat(money.multiplyBy(5.0), Matchers.is(new Money(new BigDecimal(50))));
-}
+
 
 }
->>>>>>> c10739f9d2f2ef36ab73fa9639c4c7ab001e02d8
