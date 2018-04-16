7bf94c4fc99eda560ff8aea73faea48a2d27228f
Jakub Sawicki <202289@edu.p.lodz.pl>
2018-04-16 16:18:55 +0200

    Added test addMoneyCompatibleCurrencyTest.java

--- src/test/java/lab2_2/MoneyTest.java
+++ src/test/java/lab2_2/MoneyTest.java
@@ -1,8 +1,19 @@
 package lab2_2;
 
+import static org.junit.Assert.assertThat;
+import java.math.BigDecimal;
+import java.util.Currency;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 public class MoneyTest {
 
-
+    @Test
+    public void addMoneyCompatibleCurrencyTest() {
+    	Money money=new Money(new BigDecimal(10), Currency.getInstance("EUR"));
+    	Money money2=new Money(new BigDecimal(100), Currency.getInstance("EUR"));
+    	assertThat(money.add(money2), Matchers.is(new Money(new BigDecimal(110), Currency.getInstance("EUR"))));
+    }
 
 }
